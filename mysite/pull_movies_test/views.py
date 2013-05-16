4022e0b4ca5aa5688e8ea8d9b9e8bc11902f7058
linshu123 <linshu123@gmail.com>
2013-05-16 01:42:22 -0400

    update

--- mysite/pull_movies_test/views.py
+++ mysite/pull_movies_test/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
+from mysite import settings
 
 def show_main_page(request):
-    return render_to_response('pull_movies_test/main.html')
+    return display_movies(request)
 
 def display_movies(request):
-    return render_to_response('pull_movies_test/main.html')
+    return render_to_response('pull_movies_test/main.html', {'FACEBOOK_APP_ID': settings.FACEBOOK_APP_ID})
